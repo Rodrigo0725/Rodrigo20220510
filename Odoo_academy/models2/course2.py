391075519562e064241071c5a790aae06246e1fb
Rodrigo0725 <rmeza@covalu.com>
2022-05-16 22:13:16 +0000

    [ADD]Add Models

--- Odoo_academy/models2/course2.py
+++ Odoo_academy/models2/course2.py
@@ -6,3 +6,14 @@ class Course2(models.Model):
     
     _name = 'academy.course2'
     _description = 'Course2 Info'
+    
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text(string='Description')
+    
+    level = fields.Selection(string='Level',
+                            selection=[('beginner', 'Beginner'),
+                                     ('intermediate', 'Intermediate'),
+                                     ('advanced', 'Advanced')],
+                            copy=False)
+    
+    active = fields.Boolean(string='Active', default='True')
