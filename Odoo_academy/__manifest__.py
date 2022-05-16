f955bfc05ee86b7df19f1575e6652d1ed45b5ad9
Rodrigo0725 <rmeza@covalu.com>
2022-05-16 16:39:57 +0000

    [ADD] Basic Module

---
+++ Odoo_academy/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description': """"
+        Academy Module to app manage Training:7
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    
+    'author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Trining',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data':[
+        
+    ],
+    
+    'demo':[
+        
+    ],
+}
