528a16a1747eda04f56de79ec5f27975db8532b6
Rodrigo0725 <rmeza@covalu.com>
2022-05-17 18:02:58 +0000

    [ADD] Data demo

--- Odoo_academy/models2/.ipynb_checkpoints/course2-checkpoint.py
+++ Odoo_academy/models2/.ipynb_checkpoints/course2-checkpoint.py
@@ -1,19 +1,17 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
+#-*- coding: utf- 8 -*-
+from odoo import  models, fields, api
 
 class Course2(models.Model):
     
     _name = 'academy.course2'
-    _description = 'Course2 Info'
+    _description = 'Course2 info'
     
     name = fields.Char(string='Title', required=True)
     description = fields.Text(string='Description')
-    
-    level = fields.Selection(string='Level',
-                            selection=[('beginner', 'Beginner'),
-                                     ('intermediate', 'Intermediate'),
-                                     ('advanced', 'Advanced')],
-                            copy=False)
+    level =fields.Selection(string='Level',
+                           selection =[('beginner','Beginner'),
+                                       ('intermediate','Intermediate'),
+                                       ('advanced','Advanced')],
+                           copy=False)
     
     active = fields.Boolean(string='Active', default='True')
